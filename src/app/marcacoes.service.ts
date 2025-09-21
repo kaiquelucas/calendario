30dfcd29ec53105ec22c9680078986bb7e830f3a
kaique lucas <kaiquelucas.hg@gmail.com>
2025-09-21 20:16:35 +0100

    Update marcacoes.service.ts
    melhorias

--- src/app/marcacoes.service.ts
+++ src/app/marcacoes.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Marcacoes } from './Model/marcacoes';
 import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class MarcacoesService {
-  private apiUrl = 'https://calendario-back-w81o.onrender.com/api/Marcacoes/minhas';
-  private apiUrll = `${environment.apiBaseUrl}/api/Marcacoes/minhas`;
+  private apiUrl = `${environment.apiBaseUrl}/api/Marcacoes`;
+
   constructor(private http: HttpClient) {}
 
+  private authHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return token ? new HttpHeaders({ Authorization: `Bearer ${token}` }) : new HttpHeaders();
+  }
+
   getMinhasMarcacoes(): Observable<Marcacoes[]> {
-    return this.http.get<Marcacoes[]>(`${this.apiUrl}/minhas`);
+    return this.http.get<Marcacoes[]>(`${this.apiUrl}/minhas`, { headers: this.authHeaders() });
   }
 
   criarMarcacao(marcacao: any): Observable<Marcacoes> {
-  return this.http.post<Marcacoes>(this.apiUrl, marcacao);
-}
+    return this.http.post<Marcacoes>(this.apiUrl, marcacao, { headers: this.authHeaders() });
+  }
 
   deletarMarcacoes(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.authHeaders() });
   }
 
   atualizarMarcacoes(id: number, marcacoes: Marcacoes): Observable<Marcacoes> {
-    return this.http.put<Marcacoes>(`${this.apiUrl}/${id}`, marcacoes);
+    return this.http.put<Marcacoes>(`${this.apiUrl}/${id}`, marcacoes, { headers: this.authHeaders() });
   }
 }
